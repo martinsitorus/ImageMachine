5b1d8d70d1a5881a5b5217119f37fc399536e12e
martinsitorus <sitorus.martin18@gmail.com>
2019-03-06 20:12:41 +0700

    Adding Main UI

---
+++ ImageMachine/ImageMachine/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageMachine
+//
+//  Created by Martin Sitorus on 06/03/19.
+//  Copyright © 2019 Martin Sitorus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
